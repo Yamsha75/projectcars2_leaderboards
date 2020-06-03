073b49c49c3ba8507fe60f7e7556e16630c15efd
Yamsha <jakub.lacinski@gmail.com>
2020-06-03 18:37:04 +0200

    Move db_engine and session getters to orm_base.py

--- orm_base.py
+++ orm_base.py
@@ -1,13 +1,20 @@
-from sqlalchemy import engine
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session, sessionmaker
+
+from settings import DB_CONNECT_STR
 
 Base = declarative_base()
+Engine = create_engine(DB_CONNECT_STR)
 
 
 def get_base():
     return Base
 
 
-def create_new_session(db_engine: engine):
-    return sessionmaker(bind=db_engine)()
+def get_engine():
+    return Engine
+
+
+def get_session():
+    return sessionmaker(bind=Engine, autocommit=False, autoflush=False)()
--- rebuild_db.py
+++ rebuild_db.py
@@ -1,20 +1,15 @@
-from sqlalchemy import create_engine, engine
-from sqlalchemy.orm import sessionmaker
-
 from models import Track, Vehicle, VehicleClass
-from orm_base import Base, create_new_session
-from settings import DB_CONNECT_STR
+from orm_base import get_base, get_engine, get_session
 from static_data_api import get_tracks, get_vehicle_classes, get_vehicles
 
 
-def rebuild_db(db_engine: engine):
-    session = create_new_session(db_engine)
-
+def rebuild_db():
     print("Recreating tables... ", end="")
-    Base.metadata.create_all(db_engine)
+    get_base().metadata.create_all(get_engine())
     print("Done")
 
     print("Populating tables... ")
+    session = get_session()
     table_data_getters = {
         Track: get_tracks,
         VehicleClass: get_vehicle_classes,
@@ -31,11 +26,10 @@ def rebuild_db(db_engine: engine):
         session.commit()
         print("OK")
 
-    session.end()
+    session.close()
     print("Done")
     return True
 
 
 if __name__ == "__main__":
-    engine = create_engine(DB_CONNECT_STR)
-    rebuild_db(engine)
+    rebuild_db()
