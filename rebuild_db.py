417f7ac2355579cdecc3f06544c8cce7f7b6ab6a
Yamsha <jakub.lacinski@gmail.com>
2020-06-19 09:41:39 +0200

    Implement logger usage in rebuild_db

--- rebuild_db.py
+++ rebuild_db.py
@@ -1,34 +1,31 @@
 from db import get_base, get_engine, get_session
+from logger import logger
 from models import Track, TrackedPair, Vehicle
 from settings import UPDATE_INTERVAL_HOURS
 from static_data_api import get_tracks, get_vehicles
 
 
-def printn(s: str):
-    print(s, end="", flush=True)
-
-
 def rebuild_db():
-    printn("Recreating tables... ")
+    logger.info("Started recreating tables")
     get_base().metadata.create_all(bind=get_engine())
-    print("Done")
+    logger.info("Finished recreating tables")
 
-    print("Populating tables... ")
+    logger.info("Started populating tables")
     session = get_session()
 
-    printn(f"- Populating table 'tracks'... ")
+    logger.info("Started populating table 'tracks'")
     tracks = get_tracks()
-    printn(f"Adding {len(tracks)} rows... ")
+    logger.info(f"Adding {len(tracks)} rows")
     for t in tracks:
         length_km = float(t[2]) if t[2] else None
         T = Track(id=int(t[0]), name=t[1], length_km=length_km)
         session.merge(T)
     session.commit()
-    print("OK")
+    logger.info("Finished populating table 'tracks'")
 
-    printn(f"- Populating table 'vehicles'... ")
+    logger.info("Started populating table 'vehicles'")
     vehicles = get_vehicles()
-    printn(f"Adding {len(vehicles)} rows... ")
+    logger.info(f"Adding {len(vehicles)} rows")
     for v in vehicles:
         V = Vehicle(
             id=int(v[0]),
@@ -39,12 +36,12 @@ def rebuild_db():
         )
         session.merge(V)
     session.commit()
-    print("OK")
+    logger.info("Finished populating table 'vehicles'")
 
-    printn(f"- Populating table 'tracked_pairs'... ")
+    logger.info("Started populating table 'tracked_pairs'")
     tracks = get_tracks()
     vehicles = get_vehicles()
-    printn(f"Adding {len(tracks) * len(vehicles)} rows... ")
+    logger.info(f"Adding {len(tracks) * len(vehicles)} rows")
     for track in tracks:
         ignored_track = track[3] == "1"
         for vehicle in vehicles:
@@ -53,10 +50,10 @@ def rebuild_db():
                 TP.update_interval_hours = UPDATE_INTERVAL_HOURS
             session.merge(TP)
         session.commit()
-    print("OK")
+    logger.info("Finished populating table 'tracked_pairs'")
 
     session.close()
-    print("Done")
+    logger.info("Finished populating tables")
     return True
 
 
