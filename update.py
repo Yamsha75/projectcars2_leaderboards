58c53b5957bb9316edb13e6c2d2d3bd6eab7cc37
Yamsha <jakub.lacinski@gmail.com>
2020-06-19 13:17:30 +0200

    Add update function - scrapes times for track+vehicle after last_update is later than now - updates last_update and next_update using update_interval - updates update_intervals - - if number of records is smaller than LOW_UPDATE_THRESHOLD - - if tracked player is found

---
+++ update.py
@@ -0,0 +1,44 @@
+from datetime import datetime, timedelta
+
+from sqlalchemy import or_
+
+from db import get_session
+from logger import logger
+from models import TrackedPair
+from scrape import scrape_times
+from settings import (
+    HIGH_UPDATE_INTERVAL,
+    LOW_UPDATE_INTERVAL,
+    LOW_UPDATE_THRESHOLD,
+    MID_UPDATE_INTERVAL,
+)
+
+
+def update_records(limit: int = -1):
+    session = get_session()
+    now = datetime.utcnow()
+    tracked_pairs = (
+        session.query(TrackedPair)
+        .filter(TrackedPair.update_interval_hours != None)
+        .filter(or_(TrackedPair.last_update == None, TrackedPair.next_update <= now))
+        .order_by(TrackedPair.next_update)
+        .limit(limit)
+    )
+    for tp in tracked_pairs:
+        n, found_tracked_player = scrape_times(tp.track_id, tp.vehicle_id)
+        if found_tracked_player:
+            if tp.update_interval_hours != HIGH_UPDATE_INTERVAL:
+                tp.update_interval_hours = HIGH_UPDATE_INTERVAL
+                logger.info("Found new record by tracked player")
+        elif n < LOW_UPDATE_THRESHOLD:
+            if tp.update_interval_hours != LOW_UPDATE_INTERVAL:
+                tp.update_interval_hours = LOW_UPDATE_INTERVAL
+        else:
+            if tp.update_interval_hours != MID_UPDATE_INTERVAL:
+                tp.update_interval_hours = MID_UPDATE_INTERVAL
+        tp.last_update = datetime.utcnow()
+        tp.next_update = tp.last_update + timedelta(
+            hours=tp.update_interval_hours
+        )
+        session.commit()
+    session.close()
