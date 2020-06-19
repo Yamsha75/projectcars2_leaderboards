8e6aff3f4dcb668a3cdfad49e1cdc9ed8e5edaf2
Yamsha <jakub.lacinski@gmail.com>
2020-06-19 13:19:20 +0200

    Remove code moved to update.py from main.py

--- main.py
+++ main.py
@@ -1,22 +1,4 @@
-from datetime import datetime
-
-from db import get_session
-from models import TrackedPair
-from scrape import scrape_times
-
-
-def update_tracked_pairs():
-    session = get_session()
-    t_pairs = session.query(TrackedPair).all()
-    for t_pair in t_pairs:
-        print(f"Updating lap times for {t_pair.vehicle} on {t_pair.track}...")
-        track_id = t_pair.track_id
-        vehicle_id = t_pair.vehicle_id
-        scrape_times(track_id, vehicle_id)
-        t_pair.last_update = datetime.utcnow()
-        session.commit()
-    return True
-
+from update import update_records
 
 if __name__ == "__main__":
-    update_tracked_pairs()
+    update_records(100)
